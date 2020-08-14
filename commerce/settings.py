48d96cdadbe271651ffb9eb9137ddab1eb978ac8
SUJAY S <44267241+sujay12345@users.noreply.github.com>
2020-08-14 11:40:22 +0530

    Update settings.py

--- commerce/settings.py
+++ commerce/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -118,14 +119,17 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-
-
-
-
-
-
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
+STATIC_URL = '/static/auctions/'
+MEDIA_URL = '/static/auctions/img'
+
+STATICFILES_DIRS = [  
+        os.path.join(BASE_DIR, '/static/auctions')
+]
+MEDIA_ROOT =  os.path.join(BASE_DIR, 'static/auctions/img')
+
+
 
 
