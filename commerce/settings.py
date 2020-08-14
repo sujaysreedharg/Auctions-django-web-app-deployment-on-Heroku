f70fa2dd7ac0d4b20b1b318bb9e3d5f1375e6dfb
SUJAY S <44267241+sujay12345@users.noreply.github.com>
2020-08-14 09:27:20 +0530

    Update settings.py

--- commerce/settings.py
+++ commerce/settings.py
@@ -127,7 +127,4 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'myappfolder/static'),
-)
-
+STATICFILES_DIRS = (os.path.join(BASE_DIR, '../app/static'),)
